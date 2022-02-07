ce26a5e09ad521eecdde8b4d79a1a23c5ace5cca
weiz16 <weimin1294@hotmail.com>
2022-02-07 21:42:44 +0000

    fix(build): set swcMinify to false for now

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  swcMinify: false,
   env: {
     DEV_API_URL: process.env.DEV_API_URL,
     PRODUCTION_API_URL: process.env.PRODUCTION_API_URL
