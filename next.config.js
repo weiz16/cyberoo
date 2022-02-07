f139d3bc42871075d2ded1128e07bc606a2ffa32
weiz16 <weimin1294@hotmail.com>
2022-02-07 21:53:15 +0000

    fix(build): downgrade next version

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  swcMinify: false,
   env: {
     DEV_API_URL: process.env.DEV_API_URL,
     PRODUCTION_API_URL: process.env.PRODUCTION_API_URL
